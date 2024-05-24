1ef24a9948eb1eaa87a5caeb9b4aee359370ce6f
William lynn <13thwerewolf@gmail.com>
2024-05-24 17:09:08 +0630

    Complete [CRUD]

--- StudentManagementMVC/Controllers/StudentsController.cs
+++ StudentManagementMVC/Controllers/StudentsController.cs
@@ -21,26 +21,100 @@ namespace StudentManagementMVC.Controllers
         }
 
         [HttpGet]
-        public IActionResult Add()
+        public IActionResult Add(string? id)
         {
+            //string? qID = Request.Query["id"];
+            TempData["status"] = id is null ? true : false;
+
+            if (id is not null)
+            {
+                Student? stu = new Student();
+                if (Guid.TryParse(id, out Guid gID))
+                {
+                    stu = _context.Students.Where(x => x.ID == gID).FirstOrDefault();
+                }
+                AddStudentViewModel vm = new AddStudentViewModel()
+                {
+                    ID = stu.ID,
+                    Name = stu.Name,
+                    Phone = stu.Phone,
+                    Email = stu.Email,
+                    Subed = stu.Subed
+                };
+                return View("Add", vm);
+            }
             return View();
         }
 
         [HttpPost]
         public IActionResult Add(AddStudentViewModel reqModel)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                Student stu = new Student()
+                if (reqModel.ID == Guid.Empty || reqModel.ID is null)
                 {
-                    ID = Guid.NewGuid(),
-                    Name = reqModel.Name,
-                    Email = reqModel.Email,
-                    Subed = reqModel.Subed,
-                };
-                _context.Students.Add(stu);
+                    Student stu = new Student()
+                    {
+                        ID = Guid.NewGuid(),
+                        Name = reqModel.Name,
+                        Phone = reqModel.Phone,
+                        Email = reqModel.Email,
+                        Subed = reqModel.Subed,
+                    };
+                    _context.Students.Add(stu);
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    Student? stu = _context.Students.Where(x => x.ID == reqModel.ID).FirstOrDefault();
+                    if(stu is not null)
+                    {
+                        stu.Name = reqModel.Name;
+                        stu.Phone = reqModel.Phone;
+                        stu.Email = reqModel.Email;
+                        stu.Subed = reqModel.Subed;
+                    };
+                    _context.SaveChanges();
+                    return View("AllStudents");
+                }
             }
+            TempData["status"] = true;
+            ModelState.Clear();
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult AllStudents()
+        {
+            var students = _context.Students.ToList();
+            return View(students);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            Student? stu = _context.Students.Where(x => x.ID == id).FirstOrDefault();
+            if (stu is null)
+            {
+                return View("Error");
+            }
+            _context.Students.Remove(stu);
+            _context.SaveChanges();
+            return RedirectToAction("AllStudents");
+        }
+
+        [HttpGet("Edit/{id}")]
+        public IActionResult Edit(Guid id)
+        {
+            Student? stu = _context.Students.Where(x => x.ID == id).FirstOrDefault();
+            AddStudentViewModel vm = new AddStudentViewModel()
+            {
+                Name = stu.Name,
+                Phone = stu.Phone,
+                Email = stu.Email,
+                Subed = stu.Subed
+            };
+            return View("Add", vm);
         }
     }
 }
--- StudentManagementMVC/Models/Entities/Student.cs
+++ StudentManagementMVC/Models/Entities/Student.cs
@@ -3,9 +3,9 @@
     public class Student
     {
         public Guid ID { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
         public bool Subed { get; set; }
     }
 }
